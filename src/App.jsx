34ead3fcbd6da892387be84f5b7abc9f9a1e78a0
hungtsouGL <hung.tsou@gorillalogic.com>
2022-12-13 00:43:32 -0600

    setup prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React, { createElement } from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return (
-    <div>Hello App</div>
-  )
-}
+  return <div>Hello App</div>;
+};
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(createElement(App))
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(createElement(App));
