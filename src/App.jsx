3a1041d238a95071e974957f7d9eb6ba78ca22a9
hungtsouGL <hung.tsou@gorillalogic.com>
2022-12-12 23:50:29 -0600

    setup vite with react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { createElement } from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>Hello App</div>
+  )
+}
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(createElement(App))
